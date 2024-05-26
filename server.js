f6e77793206fcb61a767e2459eaaf4627a22d18c
Akhil Bisen <bisenakhil2001@gmail.com>
2024-05-26 10:24:55 +0530

    initialized server
    initialized server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const morgan=require('morgan');
+const colors=require('colors');
+const dotenv=require('dotenv')
+//config dot env 
+dotenv.config();
+
+//rest object
+const app=express();
+
+
+//create middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+//create 
+app.get('/',(req,resp)=>{
+    resp.status(200).send(
+        {
+            message:"Server Running"
+        }
+    )
+})
+//port 
+const port=process.env.PORT;
+
+//server listening 
+app.listen(port,()=>{
+    console.log(`Server is running on ${process.env.PORT} in ${process.env.NODE_MODE} mode`.bgGreen.white)
+})
